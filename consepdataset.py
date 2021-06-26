06d83ccac3d9b02603f1490667baea6270a67e41
zhangruihan1 <47313357+zhangruihan1@users.noreply.github.com>
2021-06-27 00:49:34 +0800

    Update consepdataset.py

--- consepdataset.py
+++ consepdataset.py
@@ -5,7 +5,42 @@ import scipy.io
 import numpy as np
 import torch.nn as nn
 
-from PIL import Image
+def simpleTransform(*target, sideLength):
+	H, W = np.array(target[0].shape[:2]) - sideLength
+	assert H * W * (H + W) > 0
+	x = np.random.randint(H)
+	y = np.random.randint(W)
+
+	target = [t[x:x+sideLength, y:y+sideLength] for t in target]
+
+	if random.random() > 0.5:
+		target = [np.flip(t, 0) for t in target]
+	if random.random() > 0.5:
+		target = [np.flip(t, 1) for t in target]
+
+	return tuple([t.copy() for t in target])
+
+class ConsepSimpleTransformDataset(torch.utils.data.Dataset):
+	def __init__(self, train = False, test = False, sideLength = 512):
+		self.directory = 'CoNSeP/Train' if train else 'CoNSeP/Test'
+		self.setname = 'train' if train else 'test'
+		assert train != test
+		self.sideLength = sideLength
+
+	def __len__(self):
+		return len(os.listdir(os.path.join(self.directory, 'Images')))
+
+	def __getitem__(self, index):
+		# Load data and get label
+		image = np.array(Image.open(os.path.join(self.directory, 'Images', self.setname + f'_{index + 1}.png')))[:,:,:3]
+		labels = scipy.io.loadmat(os.path.join(self.directory, 'Labels', self.setname + f'_{index + 1}.mat'))
+		image, label_inst, label_type = simpleTransform(image, labels['inst_map'], labels['type_map'], sideLength=self.sideLength)
+
+		label_inst = torch.from_numpy(label_inst).long()
+		label_type = torch.from_numpy(label_type).long()
+		image = torch.from_numpy(np.transpose(image, (2, 0, 1))) / 255
+
+		return image, label_inst, label_type
 
 class ConsepSimpleDataset(torch.utils.data.Dataset):
 	def __init__(self, train = False, test = False):
