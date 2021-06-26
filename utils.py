5030d2bc580d90da8e7fc1690a026c99b614e3c7
zhangruihan1 <47313357+zhangruihan1@users.noreply.github.com>
2021-06-26 23:06:01 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import torch
 
 def labelRatios(iterator, index, n_classes = 8):
 	ratio = torch.ones(n_classes) # normally, it should start from zero, though we start with one to avoid 'divide by zero'
-	for batch in training_generator:
+	for batch in iterator:
 		for i in range(n_classes):
 			ratio[i] += torch.sum(batch[index] == i) # this is a super slow method, though it does not hurt so far
 	return ratio
