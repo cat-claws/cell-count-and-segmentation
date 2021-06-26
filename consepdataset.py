5fc2aebb13fc93c63e444298c80c02e12036d89c
zhangruihan1 <47313357+zhangruihan1@users.noreply.github.com>
2021-06-27 01:22:15 +0800

    Update consepdataset.py

--- consepdataset.py
+++ consepdataset.py
@@ -7,11 +7,11 @@ import torch.nn as nn
 
 from PIL import Image
 
-def simpleTransform(*target, sideLength):
-	H, W = np.array(target[0].shape[:2]) - sideLength
-	assert H * W * (H + W) > 0
-	x = np.random.randint(H)
+def simpleTransform(*target, sideLength, valid = False):
+	H, W = np.array(target[0].shape[:2])
+	x = np.random.randint(H * 0.7, H - sideLength) if valid else np.random.randint(H * 0.7 - sideLength)
 	y = np.random.randint(W)
+	assert H * W * (H + W) > 0
 
 	target = [t[x:x+sideLength, y:y+sideLength] for t in target]
 
@@ -23,20 +23,23 @@ def simpleTransform(*target, sideLength):
 	return tuple([t.copy() for t in target])
 
 class ConsepSimpleTransformDataset(torch.utils.data.Dataset):
-	def __init__(self, train = False, test = False, sideLength = 512):
+	def __init__(self, train = False, test = False, valid = False, sideLength = 256, num):
 		self.directory = 'CoNSeP/Train' if train else 'CoNSeP/Test'
 		self.setname = 'train' if train else 'test'
-		assert train != test
+		assert train != test and train != valid
 		self.sideLength = sideLength
+		self.num = num
+		self.valid = valid
 
 	def __len__(self):
-		return len(os.listdir(os.path.join(self.directory, 'Images')))
+		return self.num if not test else len(os.listdir(os.path.join(self.directory, 'Images')))
 
 	def __getitem__(self, index):
 		# Load data and get label
+		index = index % len(os.listdir(os.path.join(self.directory, 'Images')))
 		image = np.array(Image.open(os.path.join(self.directory, 'Images', self.setname + f'_{index + 1}.png')))[:,:,:3]
 		labels = scipy.io.loadmat(os.path.join(self.directory, 'Labels', self.setname + f'_{index + 1}.mat'))
-		image, label_inst, label_type = simpleTransform(image, labels['inst_map'], labels['type_map'], sideLength=self.sideLength)
+		image, label_inst, label_type = simpleTransform(image, labels['inst_map'], labels['type_map'], sideLength = self.sideLength, valid = self.valid)
 
 		label_inst = torch.from_numpy(label_inst).long()
 		label_type = torch.from_numpy(label_type).long()
