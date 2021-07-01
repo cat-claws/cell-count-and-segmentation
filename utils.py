ee568dc9fcd9080b79e2cae3485dde2011c747b3
zhangruihan1 <47313357+zhangruihan1@users.noreply.github.com>
2021-07-01 12:59:35 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,3 +6,11 @@ def labelRatios(iterator, index, n_classes = 8):
 		for i in range(n_classes):
 			ratio[i] += torch.sum(batch[index] == i) # this is a super slow method, though it does not hurt so far
 	return ratio
+
+def contour(inst_map):
+	borderExcluded = torch.nn.ZeroPad2d(1)(torch.ones((inst_map.shape[0] - 2, inst_map.shape[1] - 2))).bool()
+	c = torch.zeros_like(inst_map)
+	for i in (-1, 1):
+		for j in (-1, 1):
+			c.masked_fill_(~torch.eq(inst_map, torch.roll(inst_map, shifts=(i, j), dims=(0, 1))), 1)
+	return torch.logical_and(c.bool(), borderExcluded)
