2f2de8a0fe81ff71aa06dc71cf6f71f65b02da68
Wang Zilin <zilin_wang@mymail.sutd.edu.sg>
2021-07-05 15:14:05 +0800

    test2

--- consepdataset.py
+++ consepdataset.py
@@ -21,7 +21,6 @@ def extendLabels(force = False):
 
 			if ('hori_map' not in labels or 'vert_map' not in labels) or force:
 				map_dict = gen_targets(labels['inst_map'].astype(int), [1000, 1000])
-				print(map_dict)
 				labels['hori_map'] = map_dict['h_map']
 				labels['vert_map'] = map_dict['v_map']
 				scipy.io.savemat(os.path.join(directories[i], 'Labels', setnames[i] + f'_{index + 1}.mat'), labels)
