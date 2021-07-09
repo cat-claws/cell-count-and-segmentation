d699f081625baab7ede4e0d83731573aea330474
zhangruihan1 <47313357+zhangruihan1@users.noreply.github.com>
2021-07-09 14:26:32 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import torch
 
 def labelRatios(iterator, index, n_classes = 8):
-	ratio = torch.ones(n_classes) # normally, it should start from zero, though we start with one to avoid 'divide by zero'
+	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+	ratio = torch.ones(n_classes).to(device) # normally, it should start from zero, though we start with one to avoid 'divide by zero'
 	for batch in iterator:
 		for i in range(n_classes):
 			ratio[i] += torch.sum(batch[index] == i) # this is a super slow method, though it does not hurt so far
