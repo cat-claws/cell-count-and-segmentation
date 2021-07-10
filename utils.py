9184f3670e4f1c543a78e5e944598f20737cf410
zhangruihan1 <47313357+zhangruihan1@users.noreply.github.com>
2021-07-10 20:43:59 +0800

    add get distance

--- utils.py
+++ utils.py
@@ -8,13 +8,23 @@ def labelRatios(iterator, index, n_classes = 8):
 			ratio[i] += torch.sum(batch[index] == i) # this is a super slow method, though it does not hurt so far
 	return ratio
 
-def getEdges(inst_map):
+def getEdges(inst_map): # unluckily, we must have this compatible with torch
 	borderExcluded = torch.nn.ZeroPad2d(1)(torch.ones((inst_map.shape[-2] - 2, inst_map.shape[-1] - 2))).bool().to(inst_map.device)
 	c = torch.zeros_like(inst_map).to(inst_map.device)
 	for i in (-1, 1):
 		for j in (-1, 1):
 			c.masked_fill_(~torch.eq(inst_map, torch.roll(inst_map, shifts=(i, j), dims=(-2, -1))), 1)
 	return torch.logical_and(c.bool(), borderExcluded)
+
+from scipy import ndimage as ndi
+
+def getDistanceMap(inst_map):
+	binaries = torch.nn.functional.one_hot(torch.tensor(inst_map).long()).permute(2, 0, 1).numpy()
+	layers = []
+	for i in range(len(binaries)):
+		if np.max(binaries[i]) == 1:
+			layers.append(ndi.distance_transform_edt(binaries[i]))
+	return np.max(np.array(layers[1:]), 0)
 
 import re
 from matplotlib import pyplot as plt
