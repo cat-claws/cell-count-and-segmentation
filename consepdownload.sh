678a6d33c9636778129a36f88ada5e9aa4c64c0b
zhangruihan1 <47313357+zhangruihan1@users.noreply.github.com>
2021-07-19 14:33:39 +0800

    Update consepdownload.sh

--- consepdownload.sh
+++ consepdownload.sh
@@ -1,4 +1,5 @@
 rm -rf CoNSeP
-wget https://warwick.ac.uk/fac/cross_fac/tia/data/hovernet/consep.zip > /dev/null
+# wget https://warwick.ac.uk/fac/cross_fac/tia/data/hovernet/consep.zip > /dev/null
+python util_runtime.py
 unzip ./consep.zip > /dev/null
 rm -rf _*; rm consep.zip
