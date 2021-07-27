a2acfb9d1342f64da139b199dd772c454dc842eb
zhangruihan1 <47313357+zhangruihan1@users.noreply.github.com>
2021-07-27 17:58:56 +0800

    Update csp.py

--- csp.py
+++ csp.py
@@ -26,6 +26,25 @@ def getConstrainedMap(inst_map):
 
 	return replace_with_dict(inst_map, transdict)
 
+def getConstrainedMapNeg(inst_map):	
+	graph = nx.Graph()
+	graph.add_nodes_from(np.unique(inst_map))
+	neighbours = getNeighbours(inst_map)
+	neighbours = neighbours[:len(neighbours) // 2]
+	graph.add_edges_from(neighbours)
+	graph.remove_node(0)
+
+	transdict = nx.coloring.greedy_color(graph, strategy="random_sequential")
+
+	labels = list(range(5)) # 5 is the maximum neighbour number here
+	np.random.shuffle(labels)
+	
+	for k in transdict:
+		transdict[k] = labels[transdict[k]] + 1
+	transdict[0] = 0
+
+	return replace_with_dict(inst_map, transdict)
+
 # https://www.py4u.net/discuss/169577
 def replace_with_dict(ar, dic):
 	# Extract out keys and values
